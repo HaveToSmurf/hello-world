2eb44453cd295d380ebaa848dda5e7c5e276d8e3
HaveToSmurf <havetosmurf@hotmail.com>
2014-11-06 18:08:57 -0500

    Create main.java
    Test hello world class

---
+++ main.java
@@ -0,0 +1,14 @@
+
+/**
+ * @author Ryan
+ * Testing Github with this hello world example ;)
+ */
+public class main
+{
+
+  public static void main(String args[])
+  {
+  	System.out.println("Hello world!");
+  }
+  
+};
